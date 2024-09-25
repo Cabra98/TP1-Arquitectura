37bb470d69c9275ada02e6f6a812c2fa4a598f0d
Gian Franco Dallari <102603446+Cabra98@users.noreply.github.com>
2024-09-25 20:24:06 -0300

    Add files via upload

---
+++ project_1.srcs/sim_1/new/TestBench1.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+
+module TestBench1();
+
+ reg [3:0] operandoA;
+ reg [3:0] operandoB;
+ reg [5:0] operacion;
+ wire [3:0] resultado;
+ 
+ ALU DUT (
+ .i_operandoA(operandoA),
+ .i_operandoB(operandoB),
+ .i_operacion(operacion),
+ .o_resultado(resultado));
+ 
+ initial begin
+ 
+ operandoA=4'b0101;
+ operandoB=4'b0011;
+ operacion=6'b100000;  //suma
+#10 operacion=6'b100010;  //resta
+#10 operacion=6'b100100;  //and
+#10 operacion=6'b100101;  //or
+#10 operacion=6'b100110;  //xor
+#10 operacion=6'b000011;  //sra
+#10 operacion=6'b000010;  //srl
+#10 operacion=6'b100111;  //nor
+ 
+ end
+endmodule
---
+++ project_1.srcs/sim_1/new/proyecto1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.08.2024 19:08:10
+// Design Name: 
+// Module Name: proyecto1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module proyecto1(
+
+    );
+endmodule
